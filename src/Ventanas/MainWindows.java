5255409ab1200135037ccd78f4d5ee5f8fd433fe
LuisSalazarC <ricardo06.lsc@gmail.com>
2020-11-05 21:08:05 -0600

    cambio

--- src/Ventanas/MainWindows.java
+++ src/Ventanas/MainWindows.java
@@ -18,7 +18,6 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.Border;
-
 import Operaciones.Calculation;
 import Operaciones.Congruential;
 import Operaciones.Histogram;
